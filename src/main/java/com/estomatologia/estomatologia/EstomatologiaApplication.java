c435af695879c4d0ffac33e7826b0f0006a6c649
mkaczynski96 <kaczynskimichalkonrad@gmail.com>
2019-07-30 20:55:06 +0200

    Initial commit

---
+++ src/main/java/com/estomatologia/estomatologia/EstomatologiaApplication.java
@@ -0,0 +1,13 @@
+package com.estomatologia.estomatologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstomatologiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstomatologiaApplication.class, args);
+    }
+
+}
